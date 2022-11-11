9b43c0db5af97854e935035a26067965ca4f90b5
subin <subinstark24@gamil.com>
2022-11-11 17:13:26 +0530

    db

--- my_customers.sql
+++ my_customers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 10, 2022 at 12:15 PM
+-- Generation Time: Nov 11, 2022 at 11:38 AM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -35,20 +35,19 @@ CREATE TABLE IF NOT EXISTS `my_customers` (
   `Email_ID` varchar(100) DEFAULT NULL,
   `Passport_ID` varchar(100) DEFAULT NULL,
   `Expiry_Date` date DEFAULT NULL,
+  `deletes` varchar(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `my_customers`
 --
 
-INSERT INTO `my_customers` (`id`, `Name`, `Phone_number`, `Email_ID`, `Passport_ID`, `Expiry_Date`) VALUES
-(1, 'subin', ' + 911234567890', 'subinstark24@gmail.com', 'ageqvg', '2022-11-01'),
-(2, 'subin', ' + 11234567890', 'delhi@gmail.com', 'bhgygygr', '2022-11-29'),
-(3, 'subin', ' + 11234567890', 'hbviojb@gmail.com', 'ASDFGGA', '2022-11-29'),
-(4, 'subin', ' + 11234567890', 'subinstark24@gmail.com', 'asdfghgjkk', '2022-11-30'),
-(5, 'rahul', ' + 911234567890', 'rahul@gmail.com', 'bij8ypijnpouh', '2022-11-25'),
-(6, 'dubin', ' + 441234567890', 'subinstrk24@gmail.com', 'h bijpnb[okn[oi', '2022-11-21');
+INSERT INTO `my_customers` (`id`, `Name`, `Phone_number`, `Email_ID`, `Passport_ID`, `Expiry_Date`, `deletes`) VALUES
+(12, 'subin', ' + 919080693943', 'subinstark24@gmail.com', 'DFGHYRT', '2022-11-15', '1'),
+(13, 'subin', ' + 911234567890', 'subinstark24@gmail.com', 'CGDHJPOIOBG', '2022-11-08', '1'),
+(14, 'subin', ' + 911234567890', 'subinstark24@gmail.com', 'CGDHJPOIOBG', '2022-11-08', '0'),
+(15, 'haridhas', ' + 9691234567890', 'subinstark24@gmail.com', 'VHNJNKMKNJN', '2022-11-22', '0');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
